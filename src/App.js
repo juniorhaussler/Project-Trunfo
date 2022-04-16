260f746c27e7fca1e50f2357849003316fb903f9
juniorhaussler <juniorhaussler@gmail.com>
2022-04-16 17:38:31 -0300

    Requisito 5 concluído.

--- src/App.js
+++ src/App.js
@@ -9,32 +9,67 @@ class App extends React.Component {
 
     this.onInputChange = this.onInputChange.bind(this);
     this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
+    this.validateStateSources = this.validateStateSources.bind(this);
+    this.verifyAttr = this.verifyAttr.bind(this);
+    this.activeDesableButton = this.activeDesableButton.bind(this);
 
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: 0,
-      cardAttr2: 0,
-      cardAttr3: 0,
+      cardAttr1: '',
+      cardAttr2: '',
+      cardAttr3: '',
       imageSource: '',
-      rarity: '',
+      rarity: 'normal',
       trunfo: false,
       hasTrunfo: false,
       isSaveButtonDisabled: true,
     };
   }
 
+  onSaveButtonClick() {}
+
   onInputChange({ target }) {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
-
     this.setState({
       [name]: value,
+    }, () => {
+      const control = !this.activeDesableButton();
+      this.setState({ isSaveButtonDisabled: control });
     });
   }
 
-  onSaveButtonClick() {
-    this.setState({ isSaveButtonDisabled });
+  activeDesableButton() {
+    return this.validateStateSources() && this.verifyAttr();
+  }
+
+  validateStateSources() {
+    const { cardName, cardDescription, imageSource,
+      cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    console.log(cardName.length, cardDescription.length, imageSource.length);
+    return cardName.length > 0
+    && cardDescription.length > 0
+    && imageSource.length > 0
+    && cardAttr1.length > 0
+    && cardAttr2.length > 0
+    && cardAttr3.length > 0;
+  }
+
+  verifyAttr() {
+    const { cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    const sum = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
+    console.log(`O valor do atributo 1 é: ${cardAttr1}`);
+    console.log(`O valor do atributo 2 é: ${cardAttr2}`);
+    console.log(`O valor do atributo 3 é: ${cardAttr3}`);
+    console.log(`A soma dos atributos é: ${sum}`);
+    const zero = 0;
+    const maxAttrPoints = 90;
+    const totalPoints = 210;
+    return (cardAttr1 >= zero && cardAttr1 <= maxAttrPoints)
+    && (cardAttr2 >= zero && cardAttr2 <= maxAttrPoints)
+    && (cardAttr3 >= zero && cardAttr3 <= maxAttrPoints)
+    && (sum <= totalPoints);
   }
 
   render() {
